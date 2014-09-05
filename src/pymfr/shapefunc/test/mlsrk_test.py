cfef773df8528a1ce3560d76981812a09571e77e
Man YUAN <epsilonyuan@gmail.com>
2014-09-06 02:03:24 +0800

    improve test case coverage

--- src/pymfr/shapefunc/test/mlsrk_test.py
+++ src/pymfr/shapefunc/test/mlsrk_test.py
@@ -10,9 +10,10 @@ from pymfr.misc.weight_func import WeightFunction, InfluencRadiusBasedNodeRegula
 from pymfr.shapefunc.mlsrk import MLSRK
 from pymfr.shapefunc import mlsrk
 from pymfr.misc.kernel import PolynomialKernel
+import random
 
 
-class Test(unittest.TestCase):
+class MLSRK_Test(unittest.TestCase):
 
     def setUp(self):
         pass
@@ -23,21 +24,30 @@ class Test(unittest.TestCase):
 
 
     def test_partial_of_unity(self):
-        
-        xs=[(-0.2,),(0.7,)]
-        coords=self._oned_uniform_coords()
-        radius=self._oned_uniform_radius()
+        xs=[(-0.2,),(0.7,),(-1.8),(2.9)]
+        coords=self._oned_uniform_coords()        
         spatial_dim=1
-        
+
+        for order,r in (1,1.25),(2,1.25),(3,3):
+            radius=self._oned_uniform_radius(r)
+            self._test_partial_of_unit(xs, coords, radius, spatial_dim, order)
+    
+    def _test_partial_of_unit(self,xs,coords,radius,spatial_dim,order):
         kernel=PolynomialKernel()
-        kernel.order=1
+        kernel.order=order
         mlsrk=self.mlsrk(coords, radius, kernel)
         mlsrk.spatial_dim=spatial_dim
         node_indes = [i for i in range(len(coords))]
+        
+        xs2=xs.copy()
+        random.shuffle(xs2)
+        xs.extend(xs2)
         for x in xs:
             x=np.array(x,dtype=float)
             mlsrk.patial_order=1
-            act=mlsrk.calc(x, coords, node_indes)
+            filtered_indes=[i for i in node_indes if np.linalg.norm(x-coords[i])<radius[i]]
+            inputed_coords=coords[filtered_indes]
+            act=mlsrk.calc(x, inputed_coords, filtered_indes)
             self.assertAlmostEqual(0, np.linalg.norm(act.sum(axis=1)-(1,0)))
     
     def mlsrk(self,coords,radius,kernel):
@@ -51,9 +61,9 @@ class Test(unittest.TestCase):
     def _oned_uniform_coords(self):
         return np.linspace(-2, 3, 11).reshape((-1,1))
     
-    def _oned_uniform_radius(self):
+    def _oned_uniform_radius(self,radius=1.25):
         res= np.empty((11,1))
-        res.fill(1.25)
+        res.fill(radius)
         return res
 
 if __name__ == "__main__":
