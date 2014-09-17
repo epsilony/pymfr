54078701456a3067e74301b6b504859ee8ea565e
Man YUAN <epsilonyuan@gmail.com>
2014-09-17 21:50:08 +0800

    add 1d raw model
      (raw model is similar to PhysicalModel in Java version)

---
+++ src/pymfr/model/raw_model.py
@@ -0,0 +1,54 @@
+'''
+
+@author: "epsilonyuan@gmail.com"
+'''
+from pymfr.process.quadrature import IntervalQuadratureUnit, QuadraturePoint
+
+class MFNode:
+    
+    def __init__(self, coord=None):
+        self.coord = coord
+
+class OneDModel:
+    
+    def __init__(self, nodes=None, volume_quadrature_degree=2):
+        self.nodes = nodes
+        self.load_map = {}
+        self.dirichlet_nodes = []
+        self.neumann_nodes = []
+        self.volume_quadrature_degree = volume_quadrature_degree
+    
+    def gen_quadrature_units(self):
+        nodes = self.nodes
+        
+        degree = self.volume_quadrature_degree
+        volume_quadrature_units = []
+        for i in range(len(nodes) - 1):
+            start = nodes[i]
+            end = nodes[i + 1]
+            iqu = IntervalQuadratureUnit(None, 'volume', (start.coord, end.coord), degree)
+            volume_quadrature_units.append(iqu)
+        self.volume_quadrature_units = volume_quadrature_units
+        
+        self.neumann_quadrature_units=[]
+        for neumann_node in self.neumann_nodes:
+            self.neumann_quadrature_units.append(
+                           QuadraturePoint(None, neumann_node, 1, neumann_node.coord, neumann_node))
+        
+        self.dirichlet_quadrature_units=[]
+        for dirichlet_node in self.dirichlet_nodes:
+            self.dirichlet_quadrature_units.append(
+                           QuadraturePoint(None, dirichlet_node, 1, dirichlet_node.coord, dirichlet_node))
+    
+    def add_neumann_load(self, node, load_core):
+        self.load_map[node] = load_core
+        self.neumann_node = node
+    
+    def add_dirichlet_load(self, node, load_core):
+        self.load_map[node] = load_core
+        self.dirichlet_node = node
+    
+    def set_volume_load(self, load_core):
+        self.load_map['volume'] = load_core
+    
+    
